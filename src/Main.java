e1b620104f1b573184a5fbb487933a00219b39df
renee-lui <reneerllui@gmail.com>
2024-02-06 11:08:37 +0800

    commited

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int age;
+        String firstName;
+        String favouriteFood;
+        System.out.println("Please enter your age here:");
+        age = scan.nextInt();
+        System.out.println("Please enter your first name here:");
+        firstName = scan.next();
+        System.out.println("Please enter your favourite food here:");
+        favouriteFood = scan.next();
+        System.out.println("First name: " + firstName + "\nAge: " + age + "\nFavourite food: " + favouriteFood);
 
 
 
